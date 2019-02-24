5d64d87d71de46e700026510c03e93f3fdfe823b
trt26@case.edu <trt26@case.edu>
2019-02-23 22:15:36 -0500

    Flushed out end conditions for recursive tree building

--- node.py
+++ node.py
@@ -4,5 +4,11 @@ class Node(Object):
 		LEAF = 'leaf'
 		INTERNAL = 'internal'
 
+	@staticmethod
+    def pure_node(class_label):
+        """
+        Create a pure node which returns the given class label"""
+        return Node(node_type=Node.Type.LEAF, label=class_label)
+
 	def Node(self, node_type, children=None):
 		pass
--- tree.py
+++ tree.py
@@ -6,8 +6,7 @@ class DecisionTree(Object):
 		self.continuous_splits = {}
 		self.features = []
 
-	def set_presorted_splits(dataset, feat_index):
-		attrs_list = sorted([ex[feat_index] for ex in dataset])
+	def set_presorted_splits(self, attrs_list, feat_index):
 		self.continuous_splits[feat_index] = []
 		for i in range(len(attrs_list) - 1):
 			self.continuous_splits[feat_index].append((attrs_list[i+1] - attrs_list[i])/2.0)
@@ -16,19 +15,46 @@ class DecisionTree(Object):
 		self.features = dataset.schema
 		for index, feature in enumerate(self.features):
 			if feature.type == 'CONTINUOUS':
-				self.set_presorted_splits(dataset, index)
+				attrs_list = sorted([ex[feat_index] for ex in dataset])
+				self.set_presorted_splits(attrs_list, index)
 
 	@staticmethod
-	def _check_purity(dataset, index):
+	def _check_purity(self, dataset, index):
 		prior = None
 		for example in dataset:
 			if prior is not None or example[index] != prior:
 				return False
 			prior = example[index]
+
 		return True
 
-	def id3(dataset, class_index, remaining_features):
-		pass
+	@staticmethod
+	def _get_majority_class(self, dataset, class_index):
+		class_counts = {}
+
+		for example in dataset:
+			if example[class_index] not in class_counts
+				class_counts[example[class_index]] = 1
+			else:
+				class_counts[example[class_index]] += 1
+
+		max_count = 0
+		max_val = None
+		for key, value in class_counts.items():
+			if value > max_count:
+				max_count = value
+				max_val = key
+
+		return max_val
+
+	def _id3(self, dataset, class_index, remaining_feature_indecies):
+		if self._check_purity(dataset, class_index):
+			return Node.pure_node(dataset[0][class_index])
+		elif not remaining_features:
+			return Node.pure_node(self._get_majority_class(dataset, class_index))
+		# Else continue to finding the best split
+
+
 
 	@staticmethod
 	def parse(filepath):
